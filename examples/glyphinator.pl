1cb1c009b07183d34295710a13a1cfefcdd2433f
Dave Olszewski <cxreg@pobox.com>
2010-12-27 11:50:48 -0800

    glyphinator: add usage, verbosity

--- examples/glyphinator.pl
+++ examples/glyphinator.pl
@@ -41,11 +41,16 @@ use Games::Lacuna::Client;
 
 my %opts;
 GetOptions(\%opts,
-    'config=s',
+    'h|help',
+    'q|quiet',
+    'v|verbose',
     'db=s',
+    'config=s',
     'do-digs',
     'send-excavators',
 );
+
+usage() if $opts{h};
 
 my $glc = Games::Lacuna::Client->new(
     cfg_file => $opts{config} || "$FindBin::Bin/../lacuna.yml",
@@ -69,7 +74,7 @@ get_status();
 do_digs() if $opts{'do-digs'};
 send_excavators() if $opts{'send-excavators'} and $star_db;
 report_status();
-print "$glc->{total_calls} api calls made.\n";
+output("$glc->{total_calls} api calls made.\n");
 
 # Destroy client object prior to global destruction to avoid GLC bug
 undef $glc;
@@ -85,6 +90,7 @@ sub get_status {
 
     # Scan each planet
     for my $planet_name (keys %planets) {
+        verbose("Inspecting $planet_name\n");
         # Load planet data
         my $planet    = $glc->body(id => $planets{$planet_name});
         my $result    = $planet->get_buildings;
@@ -94,6 +100,7 @@ sub get_status {
 
         my $arch = find_arch_min($buildings);
         if ($arch) {
+            verbose("Found an archaeology ministry on $planet_name\n");
             $status->{archmin}{$planet_name} = $arch;
             my $now = time();
             my $arch_detail = $arch->view;
@@ -113,10 +120,13 @@ sub get_status {
             for my $glyph (@$glyphs) {
                 $status->{glyphs}{$glyph->{type}}++;
             }
+        } else {
+            verbose("No archaeology ministry on $planet_name\n");
         }
 
         my $spaceport = find_spaceport($buildings);
         if ($spaceport) {
+            verbose("Found a spaceport on $planet_name\n");
             $status->{spaceports}{$planet_name} = $spaceport;
 
             # How many in flight?  When arrives?
@@ -141,13 +151,21 @@ sub get_status {
 
             # How many open spots?
             $status->{open_docks}{$planet_name} = $sp_status->{docks_available} || 0;
+        } else {
+            verbose("No spaceport on $planet_name\n");
         }
 
         my @shipyards = find_shipyards($buildings);
+        verbose("No shipyards on $planet_name\n") unless @shipyards;
         for my $yard (@shipyards) {
+            verbose("Found a shipyard on $planet_name\n");
             # Make sure this yard is capable of building excavators
             my $buildable = $yard->get_buildable->{buildable};
-            next unless $buildable->{excavator}->{can};
+
+            unless($buildable->{excavator}->{can}) {
+                verbose("$planet_name is not able to build excavators yet.\n");
+                next;
+            }
 
             # Keep a record of any planet that could be building excavators, but isn't
             $status->{not_building}{$planet_name} = 1
@@ -155,6 +173,7 @@ sub get_status {
 
             # How long to build one in this shipyard?
             my $build_time = $buildable->{excavator}->{cost}->{seconds};
+            verbose("Excavators in this shipyard take " . format_time_delta_full($build_time) . "\n");
 
             # How many building?
             my $work_queue = $yard->view_build_queue;
@@ -189,50 +208,50 @@ sub report_status {
     my @events;
 
     if (keys %{$status->{glyphs} || {}}) {
-        print "Current glyphs:\n";
+        output("Current glyphs:\n");
         my $cnt;
         for my $glyph (sort keys %{$status->{glyphs}}) {
-            printf '%13s: %3s', $glyph, $status->{glyphs}->{$glyph};
-            print "\n" unless ++$cnt % 4
+            output(sprintf '%13s: %3s', $glyph, $status->{glyphs}->{$glyph});
+            output("\n") unless ++$cnt % 4
         }
-        print "\n" if $cnt % 4;
-        print "\n";
+        output("\n") if $cnt % 4;
+        output("\n");
     }
 
     # Ready to go now?
     if (my @planets = grep { $status->{ready}{$_} } keys %{$status->{ready}}) {
-        print <<END;
+        output(<<END);
 **** Notice! ****
 You have excavators ready to send.  Specify --send-excavators if you want to
 send them to the closest available destinations.
 *****************
 END
         for my $planet (@planets) {
-            print "$planet has " . pluralize($status->{ready}{$planet}, 'excavator') . " ready to launch!\n";
+            output("$planet has " . pluralize($status->{ready}{$planet}, 'excavator') . " ready to launch!\n");
         }
-        print "\n";
+        output("\n");
     }
 
     # Any idle archmins?
     if (keys %{$status->{idle}}) {
-        print <<END;
+        output(<<END);
 **** Notice! ****
 You have idle archaeology minstries.  Specify --do-digs if you want to
 start the recommended digs automatically.
 *****************
 END
         for my $planet (keys %{$status->{idle}}) {
-            print "Archaeology Ministry on $planet is idle!\n";
+            output("Archaeology Ministry on $planet is idle!\n");
         }
-        print "\n";
+        output("\n");
     }
 
     # Any planets not building excavators?
     if (my @planets = grep { $status->{not_building}{$_} } keys %{$status->{not_building}}) {
         for my $planet (@planets) {
-            print "$planet is not currently building any excavators!  It has " . pluralize($status->{open_docks}{$planet}, 'spot') . " currently available.\n";
+            output("$planet is not currently building any excavators!  It has " . pluralize($status->{open_docks}{$planet}, 'spot') . " currently available.\n");
         }
-        print "\n";
+        output("\n");
     }
 
     for my $dig (@{$status->{digs}}) {
@@ -262,22 +281,19 @@ END
         @events;
 
     if (@events) {
-        print "Upcoming events:\n";
+        output("Upcoming events:\n");
         for my $event (@events) {
             display_event($event);
         }
     }
 
-    print "\n";
+    output("\n");
 }
 
-sub format_time {
-    my $time = shift;
+sub format_time_delta {
+    my ($delta) = @_;
 
-    my $now = time();
-    my $diff = $time - $now;
-
-    given ($diff) {
+    given ($delta) {
         when ($_ < 0) {
             return "just finished";
         }
@@ -299,6 +315,41 @@ sub format_time {
     }
 }
 
+sub format_time_delta_full {
+    my ($delta) = @_;
+
+    return "just finished" if $delta <= 0;
+
+    my @formatted;
+    my $sec = $delta % 60;
+    if ($sec) {
+        unshift @formatted, format_time_delta($sec);
+        $delta -= $sec;
+    }
+    my $min = $delta % 3600;
+    if ($min) {
+        unshift @formatted, format_time_delta($min);
+        $delta -= $min;
+    }
+    my $hrs = $delta % 86400;
+    if ($hrs) {
+        unshift @formatted, format_time_delta($hrs);
+        $delta -= $hrs;
+    }
+    my $days = $delta;
+    if ($days) {
+        unshift @formatted, format_time_delta($days);
+    }
+
+    return join(', ', @formatted);
+}
+
+sub format_time {
+    my $time = shift;
+    my $delta = $time - time();
+    return format_time_delta($delta);
+}
+
 sub pluralize {
     my ($num, $word) = @_;
 
@@ -312,7 +363,7 @@ sub pluralize {
 sub display_event {
     my ($event) = @_;
 
-    printf "%15s: %s\n", $event->{when}, $event->{detail};
+    output(sprintf "%15s: %s\n", $event->{when}, $event->{detail});
 }
 
 ## Buildings ##
@@ -367,7 +418,7 @@ sub do_digs {
     for my $planet (keys %{$status->{idle}}) {
         my $ore = determine_ore($status->{available_ore}{$planet}, $status->{glyphs}, $digging);
         if ($ore) {
-            print "Starting a dig for $ore on $planet...\n";
+            output("Starting a dig for $ore on $planet...\n");
             $status->{archmin}{$planet}->search_for_glyph($ore);
             push @{$status->{digs}}, {
                 planet   => $planet,
@@ -375,7 +426,7 @@ sub do_digs {
             };
             delete $status->{idle}{$planet};
         } else {
-            print "Not starting a dig on $planet; not enough of any type of ore.\n";
+            output("Not starting a dig on $planet; not enough of any type of ore.\n");
         }
     }
 }
@@ -404,6 +455,7 @@ sub determine_ore {
 sub send_excavators {
     PLANET:
     for my $planet (grep { $status->{ready}{$_} } keys %{$status->{ready}}) {
+        verbose("Prepping excavators on $planet\n");
         my $port = $status->{spaceports}{$planet};
 
         for (1 .. $status->{ready}{$planet}) {
@@ -418,7 +470,7 @@ sub send_excavators {
                     if (my $e = Exception::Class->caught('LacunaRPCException')) {
                         if ($e->code eq '1002') {
                             # Empty orbit, update db and try again
-                            print "$dest_name is an empty orbit, trying again...\n";
+                            output("$dest_name is an empty orbit, trying again...\n");
                             mark_orbit_empty($x, $y);
                             $dest_name = undef;
                             next;
@@ -435,10 +487,10 @@ sub send_excavators {
                         # This will set the "last_excavated" time to now, which is not
                         # the case, but it's as good as we have.  It means that some bodies
                         # might take longer to get re-dug but whatever, there are others
-                        print "$dest_name was unavailable due to recent search, trying again...\n";
+                        output("$dest_name was unavailable due to recent search, trying again...\n");
                         update_last_sent($x, $y);
                     } else {
-                        print "Unknown error sending excavator from $planet to $dest_name!\n";
+                        diag("Unknown error sending excavator from $planet to $dest_name!\n");
                         next PLANET;
                     }
                     $dest_name = undef;
@@ -449,7 +501,7 @@ sub send_excavators {
                 $_->{type} eq 'excavator'
             } @{$ships->{available}};
 
-            print "Sending excavator from $planet to $dest_name...\n";
+            output("Sending excavator from $planet to $dest_name...\n");
             my $launch_status = $port->send_ship($ex->{id}, {x => $x, y => $y});
 
             if ($launch_status->{ship}->{date_arrives}) {
@@ -465,7 +517,7 @@ sub send_excavators {
 
                     update_last_sent($x, $y);
             } else {
-                print "Error sending excavator to $dest_name!\n";
+                diag("Error sending excavator to $dest_name!\n");
                 warn Dumper $launch_status;
             }
         }
@@ -481,14 +533,14 @@ sub pick_destination {
     my $x = $status->{planet_location}{$planet}{x};
     my $y = $status->{planet_location}{$planet}{y};
     my $find_dest = $star_db->prepare(<<SQL);
-select   s.name, o.orbit, o.x, o.y
+select   s.name, o.orbit, o.x, o.y, (o.x - ?) * (o.x - ?) + (o.y - ?) * (o.y - ?) as dist
 from     orbitals o
 join     stars s on o.star_id = s.id
 where    (type in ('planet', 'asteroid') or type is null)
 and      (last_excavated is null or date(last_excavated) < date('now', '-30 days'))
 and      o.x between ? and ?
 and      o.y between ? and ?
-order by (((o.x - ?) * (o.x - ?)) + ((o.y - ?) * (o.y - ?))) asc
+order by (o.x - ?) * (o.x - ?) + (o.y - ?) * (o.y - ?) asc
 limit    1
 SQL
 
@@ -496,7 +548,9 @@ SQL
     my ($dest_x, $dest_y, $dest_name);
     while (!defined $dest_x) {
         $max_dist += 100;
+        verbose("Increasing box size to " . ($max_dist * 2) . "\n");
         $find_dest->execute(
+            $x, $x, $y, $y,
             $x - $max_dist,
             $x + $max_dist,
             $y - $max_dist,
@@ -508,6 +562,9 @@ SQL
             $dest_x = $row->{x};
             $dest_y = $row->{y};
             $dest_name = "$row->{name} $row->{orbit}";
+
+            my $dist = int(sqrt($row->{dist}));
+            verbose("Selected destination $dest_name, which is $dist units away\n");
         }
     }
 
@@ -519,7 +576,7 @@ sub update_last_sent {
 
     my $r = $star_db->do(q{update orbitals set last_excavated = datetime(?,'unixepoch') where x = ? and y = ?}, {}, time(), $x, $y);
     unless ($r > 0) {
-        print STDERR "Warning: could not update orbitals table for body at $x, $y!\n";
+        diag("Warning: could not update orbitals table for body at $x, $y!\n");
     }
 }
 
@@ -528,6 +585,51 @@ sub mark_orbit_empty {
 
     my $r = $star_db->do(q{update orbitals set type = 'empty' where x = ? and y = ?}, {}, $x, $y);
     unless ($r > 0) {
-        print STDERR "Warning: could not update orbitals table for body at $x, $y!\n";
+        diag("Warning: could not update orbitals table for body at $x, $y!\n");
     }
+}
+
+sub usage {
+    diag(<<END);
+Usage: $0 [options]
+
+This program will manage your glyph hunting worries with minimal manual
+intervention required.  It will notice archeology digs, ready-to-launch
+excavators, and idle shipyards and notify you of them.  It can start digs
+for the most needed glyphs, and send excavators to the nearest available
+bodies.
+
+This is suitable for automation with cron(8) or at(1), but you should
+know that it tends to use a substantial number of API calls, often 50-100
+per run.  With the daily limit of 5000, including all web UI usage, you
+will want to keep these at a relatively infrequent interval, such as every
+60 minutes at most.
+
+Options:
+  --verbose          - Output extra information.
+  --quiet            - Print no output except for errors.
+  --config <file>    - Specify a GLC config file, normally lacuna.yml.
+  --db <file>        - Specify a star database, normally stars.db.
+  --do-digs          - Begin archaeology digs on any planets which are idle.
+  --send-excavators  - Launch ready excavators at their nearest destinations.
+                       The information for these is selected from the star
+                       database, and the database is updated to reflect your
+                       new searches.
+END
+    exit 1;
+}
+
+sub verbose {
+    return unless $opts{v};
+    print @_;
+}
+
+sub output {
+    return if $opts{q};
+    print @_;
+}
+
+sub diag {
+    my ($msg) = @_;
+    print STDERR $msg;
 }
